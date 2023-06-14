ea02e85f937884216b6f4707d5f8b27e01cde434
Rosaline <130102218+Shaggi123@users.noreply.github.com>
2023-06-14 10:18:42 -0700

    Add files via upload
     A website in Visual Studio

---
+++ contoso craft2/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+// script.js
+window.addEventListener('load', function() {
+  var box = document.querySelector('.box');
+  box.style.opacity = 0; // Set initial opacity to 0
+
+  // Animate fading effect
+  var opacity = 0;
+  var interval = setInterval(function() {
+    if (opacity >= 1) {
+      clearInterval(interval);
+    }
+    opacity += 0.01;
+    box.style.opacity = opacity;
+  }, 10);
+});
